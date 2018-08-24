e359a2d12b588ecd2b4e421fa9fb82809b14adfc
aleksanderAnferoff <aleksander.anferoff@ya.ru>
2018-08-24 14:27:35 +0500

    correct require

--- app.rb
+++ app.rb
@@ -2,12 +2,13 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
-require 'sinatra-activerecord'
+require 'sinatra/activerecord'
 
 set :datebase, "sqlite3:lesson29.db"
 
 class Client < ActiveRecord::Base
 end
+
 
 get '/' do
 	erb "Hello!"
